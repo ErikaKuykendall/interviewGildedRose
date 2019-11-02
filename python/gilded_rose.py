071cab18fe06275dd7061bc3d3bff29ba00db67f
Erika Kuykendall <Erika.Kuykendall@protonmail.com>
2019-11-02 11:14:52 -0400

    Another -=

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -22,7 +22,7 @@ class GildedRose(object):
             else:
                 if item.quality > 0:
                     item.quality -= 1
-            item.sell_in = item.sell_in - 1
+            item.sell_in -= 1
             if item.sell_in < 0:
                 if item.name == "Aged Brie":
                     if item.quality < 50:
