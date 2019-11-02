c931ece267c9d6087be5f11ae8ad11229cf71241
Erika Kuykendall <Erika.Kuykendall@protonmail.com>
2019-11-02 10:18:34 -0400

    Since all checking for Sulfuras ever does is check to see if we should do nothing, just do nothing as soon as possible.

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -7,6 +7,8 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
+            if item.name == "Sulfuras, Hand of Ragnaros":
+                continue
             if item.name == "Aged Brie" or item.name == "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality < 50:
                     item.quality = item.quality + 1
@@ -19,14 +21,8 @@ class GildedRose(object):
                                 item.quality = item.quality + 1
             else:
                 if item.quality > 0:
-                    if item.name == "Sulfuras, Hand of Ragnaros":
-                        pass
-                    else:
-                        item.quality = item.quality - 1
-            if item.name == "Sulfuras, Hand of Ragnaros":
-                pass
-            else:
-                item.sell_in = item.sell_in - 1
+                    item.quality = item.quality - 1
+            item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
                 if item.name == "Aged Brie":
                     if item.quality < 50:
@@ -36,10 +32,7 @@ class GildedRose(object):
                         item.quality = item.quality - item.quality
                     else:
                         if item.quality > 0:
-                            if item.name == "Sulfuras, Hand of Ragnaros":
-                                pass
-                            else:
-                                item.quality = item.quality - 1
+                            item.quality = item.quality - 1
 
 
 class Item:
