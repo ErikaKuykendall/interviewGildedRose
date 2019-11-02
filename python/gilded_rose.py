6da8420517c2e28fa8ea0f8b618c7ac74c919804
Erika Kuykendall <Erika.Kuykendall@protonmail.com>
2019-11-02 11:38:17 -0400

    Clean up the logic in each case by removing redundant if-statements and reordering things slightly

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -11,31 +11,22 @@ class GildedRose(object):
                 continue
             elif item.name == "Aged Brie":
                 item.sell_in -= 1
-                if item.quality < 50:
-                    item.quality += 1
-                if item.sell_in < 0:
-                    if item.name == "Aged Brie":
-                        if item.quality < 50:
-                            item.quality += 1
+                item.quality += 2 if item.sell_in < 0 else 1
+                if item.quality > 50: item.quality = 50
             elif item.name == "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality < 50:
+                item.quality += 1
+                if item.sell_in < 11:
+                     item.quality += 1
+                if item.sell_in < 6:
                     item.quality += 1
-                    if item.sell_in < 11:
-                         if item.quality < 50:
-                             item.quality += 1
-                    if item.sell_in < 6:
-                        if item.quality < 50:
-                            item.quality += 1
                 item.sell_in -= 1
                 if item.sell_in < 0:
-                    item.quality -= item.quality
+                    item.quality = 0
+                if item.quality > 50: item.quality = 50
             else:
-                if item.quality > 0:
-                    item.quality -= 1
                 item.sell_in -= 1
-                if item.sell_in < 0:
-                    if item.quality > 0:
-                        item.quality -= 1
+                item.quality -= 2 if item.sell_in < 0 else 1
+                if item.quality < 0: item.quality = 0
 
 
 class Item:
