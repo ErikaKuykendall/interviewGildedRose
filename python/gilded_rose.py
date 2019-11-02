35d4be6ef02a70a7f37b90bc95bb58a9a3aadd0b
Erika Kuykendall <Erika.Kuykendall@protonmail.com>
2019-11-02 11:24:55 -0400

    Split the logic between the different types of items, rather than checking what kind of item is being worked on every step of the way.

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -9,30 +9,33 @@ class GildedRose(object):
         for item in self.items:
             if item.name == "Sulfuras, Hand of Ragnaros":
                 continue
-            if item.name == "Aged Brie" or item.name == "Backstage passes to a TAFKAL80ETC concert":
+            elif item.name == "Aged Brie":
+                item.sell_in -= 1
                 if item.quality < 50:
                     item.quality += 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality += 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality += 1
+                if item.sell_in < 0:
+                    if item.name == "Aged Brie":
+                        if item.quality < 50:
+                            item.quality += 1
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                if item.quality < 50:
+                    item.quality += 1
+                    if item.sell_in < 11:
+                         if item.quality < 50:
+                             item.quality += 1
+                    if item.sell_in < 6:
+                        if item.quality < 50:
+                            item.quality += 1
+                item.sell_in -= 1
+                if item.sell_in < 0:
+                    item.quality -= item.quality
             else:
                 if item.quality > 0:
                     item.quality -= 1
-            item.sell_in -= 1
-            if item.sell_in < 0:
-                if item.name == "Aged Brie":
-                    if item.quality < 50:
-                        item.quality += 1
-                else:
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        item.quality -= item.quality
-                    else:
-                        if item.quality > 0:
-                            item.quality -= 1
+                item.sell_in -= 1
+                if item.sell_in < 0:
+                    if item.quality > 0:
+                        item.quality -= 1
 
 
 class Item:
